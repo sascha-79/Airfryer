32ae2d5ac7aeb03266a301fcf449ad89c4b35c13
sascha-79 <luinba@bluewin.ch>
2026-09-04 22:37:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='airfry-v1.4-usability';
+const CACHE='airfry-v1.4.1-weekpicker';
 const CORE=['./','./index.html','./manifest.json','./recipes.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
